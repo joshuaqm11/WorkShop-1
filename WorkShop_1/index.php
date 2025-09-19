e6a45b8a68fd7bffdf223b98d50c5e3dd6afaac1
joshuaqm11 <167342009+joshuaqm11@users.noreply.github.com>
2025-09-19 10:22:40 -0600

    Update index.php

--- WorkShop_1/index.php
+++ WorkShop_1/index.php
@@ -1,5 +1,8 @@
 <?php
-date_default_timezone_set("America/Costa_Rica"); // Ajusta a tu zona horaria
-echo "<h1>Fecha y hora actual en PHP</h1>";
-echo "<p>" . date("d/m/Y H:i:s") . "</p>";
+date_default_timezone_set("America/Costa_Rica");
+
+echo "<h1>WorkShop #1</h1>"; 
+echo "<h2>Fecha y hora actual en PHP</h2>";
+echo "<p>" . date("d/m/Y H:i:s") . "</p>"; 
+echo "<p><strong>Nombre:</strong> Joshua Quesada Madrigal</p>"; 
 ?>
